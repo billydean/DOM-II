0e2ac63e0ce80c5ea30c7af2f0764113de5c755f
billydean <billy.goehring@gmail.com>
2022-02-08 11:19:11 -0800

    set out goals and steps necessary for MVP tasks in comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,81 @@
 import './less/index.less'
 
-// Your code goes here!
+// Create listeners of 10 types of events
+/* some examples:
+ - mouseover
+ - keydown
+ - wheel
+ - load
+ - focus
+ - resize
+ - scroll
+ - select
+ - dblclick
+ - drag / drop
+
+ OTHERS:
+  - gestures (touch events)
+  - 
+*/
+
+/* 
+My listeners! (ML1 - ML10)
+1-3 mouseover first "sign me up" button; changes picture and text for "pick your destination" section. Ditto for second and third buttons
+4. zoom on pictures if WHEEL while mouseover PREVENT DEFAULT??
+5. drag/drop picture to display picture on the header image
+6. scrolling down transitions some colors
+7. mouseover nav items makes them bold
+8. select makes italic
+9. on load, add field to drag/drop picture
+10. on load, add all appropriate CSS classes for what I want to do
+
+*/
+
+// Necessary selectors based on ML1-10
+//ML1
+    // button number 1
+    // destination header
+    // destination picture
+    // destination text
+
+//ML2
+    // button number 2
+//ML3
+    // button number 3
+//ML4
+    // image class
+//ML5
+    // input field to be created
+//ML6
+    // all text 
+//ML7
+    // nav text
+//ML8
+    // all text (see ML6)
+//ML9
+    // where new div will be inserted (see ML5)
+//ML10
+    // ANYTHING CHANGING STYLE ABOVE??
+    // image class
+
+// ML1-10 functions & listeners
+//ML1
+    // listen for mouseover
+    // function: change dest header, picture, text
+//ML2
+    // listen for mouseover
+    // function: change dest header, picture, text
+//ML3
+    // listen for mouseover
+    // function: change dest header, picture, text
+//ML4
+//ML5
+//ML6
+//ML7
+//ML8
+//ML9
+//ML10
+
+// Find a use case for preventDefault. For example, you could prevent a link from navigating when clicked, or to navigate somewhere surprising.
+
+// You'll submit link to github repo on canvas
