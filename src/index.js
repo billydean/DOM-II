27f051f7cd78b49504ab02553798f78f97a9d7ba
billydean <billy.goehring@gmail.com>
2022-02-08 13:32:39 -0800

    really struggled with the new format of this project (how the script was bundled, etc). A hot mess all around

--- src/index.js
+++ src/index.js
@@ -18,64 +18,69 @@ import './less/index.less'
   - 
 */
 
-/* 
-My listeners! (ML1 - ML10)
-1-3 mouseover first "sign me up" button; changes picture and text for "pick your destination" section. Ditto for second and third buttons
-4. zoom on pictures if WHEEL while mouseover PREVENT DEFAULT??
-5. drag/drop picture to display picture on the header image
-6. scrolling down transitions some colors
-7. mouseover nav items makes them bold
-8. select makes italic
-9. on load, add field to drag/drop picture
-10. on load, add all appropriate CSS classes for what I want to do
-
-*/
 
 // Necessary selectors based on ML1-10
 //ML1
     // button number 1
-    // destination header
-    // destination picture
-    // destination text
+document.addEventListener('DOMContentLoaded', ()=>{
+        const buttons = Array.from(document.querySelectorAll('div.btn'));
+        buttons[0].classList.add('funSun');
+        buttons[1].classList.add('mountain');
+        buttons[2].classList.add('island');
+        const imageArray = Array.from(document.querySelectorAll('img'));
+        for (let i=0; i<imageArray.length; i++) {
+            let string = 'img';
+            let num = 1 + i;
+            imageArray[i].id = string + num;
+        }
+        const destinationText = document.querySelector('.content-destination p');
+const destinationImg = document.querySelector('.content-destination img');
+const sunButton = document.querySelector('div.btn.funSun');
 
-//ML2
-    // button number 2
-//ML3
-    // button number 3
-//ML4
-    // image class
-//ML5
-    // input field to be created
-//ML6
-    // all text 
-//ML7
-    // nav text
-//ML8
-    // all text (see ML6)
-//ML9
-    // where new div will be inserted (see ML5)
-//ML10
-    // ANYTHING CHANGING STYLE ABOVE??
-    // image class
+sunButton.addEventListener('mouseover', function(){
+    destinationImg.src = "http://localhost:9000/img/imageofsunnyplace.jpg";
+    destinationText.textContent = "Here's a description of an awfully sunny place!";
+});
 
-// ML1-10 functions & listeners
-//ML1
-    // listen for mouseover
-    // function: change dest header, picture, text
-//ML2
-    // listen for mouseover
-    // function: change dest header, picture, text
-//ML3
-    // listen for mouseover
-    // function: change dest header, picture, text
-//ML4
-//ML5
-//ML6
-//ML7
-//ML8
-//ML9
-//ML10
+const mountainButton = document.querySelector('.mountain');
+mountainButton.addEventListener('mouseover', function(){
+    destinationImg.src = "http://localhost:9000/img/imageofmountains.jpg";
+    destinationText.textContent = "Here's a description of some really pretty mountains!";
+});
+const islandButton = document.querySelector('.island');
+islandButton.addEventListener('mouseover', function(){
+    destinationImg.src = "http://localhost:9000/img/imageofislandparadise.jpg";
+    destinationText.textContent = "Your next vacation could be here on these great islands. Very pretty.";
+});
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('mouseover', (event) => {
+        event.target.style.fontWeight = 'bolder';
 
-// Find a use case for preventDefault. For example, you could prevent a link from navigating when clicked, or to navigate somewhere surprising.
-
-// You'll submit link to github repo on canvas
+    })
+    link.addEventListener('mouseout', (event) => {
+        event.target.style.fontWeight = 'normal';
+    })
+});
+// I wanted the pictures to zoom but stay cropped by maximum size of their containing divs. I think it'd take a LOT more fooling around with the css/html to pull that off.
+let scale = 1; 
+document.querySelectorAll('.img-content').forEach(picture => {
+    picture.style.objectFit = 'cover';
+});
+document.querySelectorAll('img').forEach(picture => {
+    picture.addEventListener('wheel', (event)=>{
+        event.target.style.objectFit = 'cover';
+        event.target.style.minWidth = '105%';
+        scale += event.deltaY * -0.01;
+        scale = Math.min(Math.max(0.125, scale),4);
+        event.target.style.transform = `scale(${scale})`;
+    }, {passive: true});
+    picture.addEventListener('mouseout', (event) => {
+        event.target.style.transform = `scale(1)`;
+    } )
+});
+document.querySelectorAll('p').forEach(function (text) {
+    text.addEventListener('select', (event) => {
+        event.target.style.fontWeight = 'bold';
+    });
+});
+    })
